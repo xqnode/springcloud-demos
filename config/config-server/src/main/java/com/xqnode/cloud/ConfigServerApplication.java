f601341c3a682c09ef4f4ec100294f43f39333c1
xqnode <xqnode@163.com>
2019-10-21 17:09:21 +0800

    配置文件映射规则

--- config/config-server/src/main/java/com/xqnode/cloud/ConfigServerApplication.java
+++ config/config-server/src/main/java/com/xqnode/cloud/ConfigServerApplication.java
@@ -1,11 +1,13 @@
 package com.xqnode.cloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication //(exclude={DataSourceAutoConfiguration.class})  //禁止启动检查数据源
 @EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
